b474b41dc30f00d483bf78bb0b13be33f9de0fc4
Ben Pollack <benpollack8723@gmail.com>
2024-07-25 10:49:27 -0500

    GET request and table rendered

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,46 @@
-console.log( 'js' );
+console.log('js');
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
+function getKoalas() {
+  console.log('in getKoalas');
   // axios call to server to get koalas
-  
-} // end getKoalas
+  axios({
+    method: 'GET',
+    url: '/koalas'
+  }).then(function (response) {
+    console.log('getKoalas() response', response.data);
+    renderKoalas(response.data);
+  }).catch(function (error) {
+    console.log('error in GET', error);
+  });
+}; // end getKoalas
 
-function saveKoala(){
-  console.log( 'in saveKoala' );
+function saveKoala() {
+  console.log('in saveKoala');
   // axios call to server to get koalas
- 
+
 }
 
 getKoalas();
+
+
+function renderKoalas(koalas) {
+  let viewKoalasTable = document.getElementById('viewKoalas');
+
+  for (let koala of koalas) {
+    viewKoalasTable.innerHTML += (`
+  <tr>
+    <td>${koala.name}</td>
+    <td>${koala.age}</td>
+    <td>${koala.favorite_color}</td>
+    <td>${koala.ready_to_transfer}</td>
+    <td>${koala.notes}</td>
+    <td>
+      <button onclick="">Ready For Transfer</button>
+    </td>
+    <td>
+      <button onclick="">Delete</button>
+    </td>
+  </tr>
+  `)
+  }
+}
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,21 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
-
+koalaRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koalas";';
+    
+    pool.query(queryText).then(result => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    })
+      .catch(error => {
+        console.log('error getting koalas', error);
+        res.sendStatus(500);
+      });
+  });
 
 // POST
 
